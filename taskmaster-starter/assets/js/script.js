84c273f40fd7aad3fa2aab61156d5dbfa71c1ba6
Zoey <jsisavath@gmail.com>
2022-03-21 22:42:39 -0500

    changed fx orders in js.script doc for readability

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -45,39 +45,8 @@ var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-$(".list-group").on("click", "p", function () {
-  var text = $(this)
-    .text()
-    .trim();
 
-  var textInput = $("<textarea>")
-    .addClass("form-control")
-    .val(text);
 
-  $(this).replaceWith(textInput);
-  textInput.trigger("focus");
-});
-
-$(".list-group").on("blur", "textarea", function () {
-  // get the textarea's current value/text
-  var text = $(this)
-    .val()
-    .trim();
-
-  // get the parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
-
-  // get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
-
-  tasks[status][index].text = text;
-  saveTasks();
-});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function () {
@@ -111,15 +80,52 @@ $("#task-form-modal .btn-primary").click(function () {
 
     saveTasks();
   }
+});
+// task text was clicked
+$(".list-group").on("click", "p", function () {
+  // get current text of p element
+  var text = $(this)
+    .text()
+    .trim();
+// replace p element with a new textarea
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+// auto focus new element
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
+
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function () {
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+// update task in array and re-save to localstorage
+  tasks[status][index].text = text;
+  saveTasks();
+  
   // recreate p element
   var taskP = $("<p>")
     .addClass("m-1")
     .text(text);
 
-  // replace textarea with p element
+  // replace textarea with new element
   $(this).replaceWith(taskP);
-
 });
+
 
 // due date was clicked
 $(".list-group").on("click", "span", function () {
@@ -137,7 +143,7 @@ $(".list-group").on("click", "span", function () {
   // swap out elements
   $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
+   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
